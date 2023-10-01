fa3f5924b2f67564c2a91721a1fad29cba145df9
pachecodv02 <pacheco.8727@gmail.com>
2023-09-30 23:09:48 -0600

    Se creo la clase personaRegsitro en la rama main

---
+++ src/com/prueba/PersonaRegistro.java
@@ -0,0 +1,13 @@
+package com.prueba;
+
+public class PersonaRegistro {
+    private String nombre, apellidos;
+
+    @Override
+    public String toString() {
+        return "PersonaRegistro{" +
+                "nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                '}';
+    }
+}
